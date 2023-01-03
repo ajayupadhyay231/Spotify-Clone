63f949c7ed7f8bfc47b3186552ec1c7b8d889186
Ajay <ajayupadhyay20000@gmail.com>
2023-01-03 13:49:27 +0530

    updates

--- script.js
+++ script.js
@@ -10,24 +10,35 @@ let masterSongName = document.getElementById('masterSongName');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 let songs = [
-    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
-    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
-    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
-    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
-    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
-    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
-    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
-    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
-    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
-    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+    {songName: "AAGE CHAL- RAFTAAR", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "ON MY WAY - ALAN WALKER", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "FADED - ALAN WALKER", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "PASOORI - ALI SETHI x SHAE GILL", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "DANVEER KARAN THE - ABBY VIRAL", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "BIJLEE BIJLEE - HARRDY SANDHU ", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "KINNA SONA - JUBIN NAUTIYAL ", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "BELIEVER - IMAGINE DRAGONS ", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "KHWAHISH - MUNAWAR FARUQUI", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "MANN MERI JANN- KING", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
 ]
+
 
 songItems.forEach((element, i)=>{ 
     element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
     element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
 })
  
-
+// Add a click event listener to the "About" button
+document.getElementById('about-button').addEventListener('click', function() {
+    // Toggle the visibility of the about section
+    var aboutSection = document.getElementById('about-section');
+    if (aboutSection.style.display === 'block') {
+      aboutSection.style.display = 'none';
+    } else {
+      aboutSection.style.display = 'block';
+    }
+  });
+  
 // Handle play/pause click
 masterPlay.addEventListener('click', ()=>{
     if(audioElement.paused || audioElement.currentTime<=0){
